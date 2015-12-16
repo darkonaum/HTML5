232dca45f019bbbc3bb4b0f227270988c81cd199
darkonaum <darkonaum@gmial.com>
2015-12-16 12:40:30 +0100

    JS 1 semos

---
+++ javascript/js1.js
@@ -0,0 +1,40 @@
+alert('Паднав на тестот по HTML и CSS');
+console.log('чекор по чекор.... не се од еднаш');
+
+typeNum = 1234;
+typeNum2 = 3.14;
+
+typeBool = true;
+typeBool2 = false;
+
+typeObj = {};
+
+typeStr = "bla bla bla bla 123456765756 *($^#%(*&$(*#@)($*";
+
+var1 = "123";
+var2 = 123;
+
+ime = 'Bojan';
+console.log(ime);
+
+a = 34;
+b = 12;
+
+c = a * b;
+console.log(c);
+
+console.log(10 % 3);
+console.log(15 % 4);
+console.log(21 % 2);
+
+C = 12;
+console.log(C * 9 / 5 + 32);
+
+F = 53.6;
+console.log((F - 32) * 5 / 9);
+
+function c2f(c) {
+	return c * 9 / 5 + 32
+}
+
+console.log(c2f(43));
